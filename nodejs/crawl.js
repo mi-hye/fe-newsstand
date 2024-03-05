01b1367e7061a29ab4dadedbd935a5ba0af88ef8
mi-hye <satge13@gmail.com>
2024-03-05 14:44:21 +0900

    feat: Implement getImgsElements

--- nodejs/crawl.js
+++ nodejs/crawl.js
@@ -1,16 +1,13 @@
 import puppeteer from "puppeteer";
 import { setInterval } from "timers/promises";
 
-async function crawl() {
-	const latestNews = [];
-	const browser = await puppeteer.launch({ headless: false });
-	const page = await browser.newPage();
+const getImgElements = async (page) =>
+	await page.evaluate(() => {
+		const imgNodes = document.querySelectorAll(".news_logo");
+		return Array.from(imgNodes).map((img) => img.src);
+	});
 
-	await page.goto("https://www.naver.com/");
-
-	//nodejs에서는 dom엘리먼트에 접근을 못한다
-	//함수를 만들어서 브라우저에게 전달
-	//브라우저가 함수를 실행시켜서 그 결과를 node에서 사용
+const getLatestNews = async (page, latestNews) => {
 	for await (const startTime of setInterval(3000, Date.now())) {
 		const now = Date.now();
 		const el = await page.waitForSelector(
@@ -22,13 +19,34 @@ async function crawl() {
 		latestNews.push({ text, href });
 
 		if (now - startTime >= 33000) {
-			browser.close();
 			break;
 		}
 	}
+};
 
-	return latestNews;
+async function crawl() {
+	const latestNews = [];
+	const imgs = [];
+	const browser = await puppeteer.launch({ headless: false });
+	const page = await browser.newPage();
+
+	await page.goto("https://www.naver.com/");
+
+	for (let i = 0; i < 4; i++) {
+		imgs.push(await getImgElements(page));
+		await page.click(".ContentPagingView-module__btn_next___ZBhby");
+	}
+
+	//nodejs에서는 dom엘리먼트에 접근을 못한다
+	//함수를 만들어서 브라우저에게 전달
+	//브라우저가 함수를 실행시켜서 그 결과를 node에서 사용
+	await getLatestNews(page, latestNews);
+	browser.close();
+
+	latestNews.shift();
+	return [latestNews, imgs];
 }
 
-const b = await crawl();
+const [a, b] = await crawl();
+console.log("출력", a);
 console.log("출력", b);
