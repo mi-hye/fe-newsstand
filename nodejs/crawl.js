9c9419f9ee2f4c7e2e910a661de87f22b7dd9b7d
mi-hye <satge13@gmail.com>
2024-03-05 14:59:38 +0900

    refactor: Seperate Fn

--- nodejs/crawl.js
+++ nodejs/crawl.js
@@ -7,39 +7,34 @@ const getImgElements = async (page) =>
 		return Array.from(imgNodes).map((img) => img.src);
 	});
 
-const getLatestNews = async (page, latestNews) => {
+const getLatestNews=async(page, latestNews)=> {
 	for await (const startTime of setInterval(3000, Date.now())) {
 		const now = Date.now();
-		const el = await page.waitForSelector(
-			".ContentHeaderSubView-module__news_title___wuetX",
-			{ timeout: 500 }
-		);
-		const text = await el.$eval("a", (el) => el.innerText);
-		const href = await el.$eval("a", (el) => el.href);
-		latestNews.push({ text, href });
-
+		const news = await page.evaluate(() => {
+			const a = document.querySelector(
+				".ContentHeaderSubView-module__news_title___wuetX > a"
+			);
+			return { text: a.text, href: a.href };
+		});
+		
+		latestNews.push(news);
 		if (now - startTime >= 33000) {
 			break;
 		}
 	}
-};
+}
 
 async function crawl() {
 	const latestNews = [];
 	const imgs = [];
 	const browser = await puppeteer.launch({ headless: false });
 	const page = await browser.newPage();
-
 	await page.goto("https://www.naver.com/");
 
 	for (let i = 0; i < 4; i++) {
 		imgs.push(await getImgElements(page));
 		await page.click(".ContentPagingView-module__btn_next___ZBhby");
 	}
-
-	//nodejs에서는 dom엘리먼트에 접근을 못한다
-	//함수를 만들어서 브라우저에게 전달
-	//브라우저가 함수를 실행시켜서 그 결과를 node에서 사용
 	await getLatestNews(page, latestNews);
 	browser.close();
 
