913ed1ed524ac26ecb37aa030b393e54947b537b
mi-hye <satge13@gmail.com>
2024-03-07 12:22:59 +0900

    refactor: Seperate Function

--- js/latest-news.js
+++ js/latest-news.js
@@ -4,47 +4,46 @@ const news = await readJSON("news");
 
 const firstNews = news.slice(0, 5);
 const secondNews = news.slice(5, 10);
-
 const [firstArea, secondArea] = document.querySelectorAll(
 	".latest-news__rolling"
 );
 
-function createLatestNews(idx) {
-	firstArea.innerHTML = `
-    <a href="${firstNews[idx].href}" class="latest-news__rolling--befor-hover">
-    ${firstNews[idx].text}
-    </a>
-    `;
-}
+AddLatestNews(firstArea, firstNews);
+AddLatestNews(secondArea, secondNews);
 
-function startNewChanging() {
-	interval = setInterval(() => {
-		if (idx === 4) idx = 0;
-		else idx += 1;
-		createLatestNews(idx);
-	}, 4000);
-}
+function AddLatestNews(rollingArea, news) {
+	let interval;
+	let idx = 0;
 
-function stopNewChanging() {
-	clearInterval(interval);
-	const currentNews = document.querySelector(
-		".latest-news__rolling > a"
-	);
-	currentNews.classList = "latest-news__rolling--stop";
-}
+	const createLatestNews = (idx) => {
+		rollingArea.innerHTML = `
+		<a href="${news[idx].href}" class="latest-news__rolling--befor-hover">
+		${news[idx].text}
+		</a>
+		`;
+	};
 
-firstArea.addEventListener("mouseleave", () => {
-	console.log("아웃");
-	const currentNews = document.querySelector(
-		".latest-news__rolling--stop"
-	);
-	currentNews.classList = "latest-news__rolling--after-hover";
+	const startNewChanging = () => {
+		interval = setInterval(() => {
+			if (idx === 4) idx = 0;
+			else idx += 1;
+			createLatestNews(idx);
+		}, 4000);
+	};
+
+	const stopNewChanging = () => {
+		clearInterval(interval);
+		const currentNews = rollingArea.querySelector("a");
+		currentNews.classList = "latest-news__rolling--stop";
+	};
+
+	rollingArea.addEventListener("mouseleave", () => {
+		const currentNews = rollingArea.querySelector("a");
+		currentNews.classList = "latest-news__rolling--after-hover";
+		startNewChanging();
+	});
+	rollingArea.addEventListener("mouseenter", stopNewChanging);
+
+	createLatestNews(idx);
 	startNewChanging();
-});
-firstArea.addEventListener("mouseenter", stopNewChanging);
-
-let interval;
-let idx = 0;
-
-createLatestNews(idx);
-startNewChanging();
+}
