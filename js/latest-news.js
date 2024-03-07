92265cd3bbdb83e200d552cf1d363296ac76e8d1
mi-hye <satge13@gmail.com>
2024-03-07 11:36:13 +0900

    feat: Implement news rolling

--- js/latest-news.js
+++ js/latest-news.js
@@ -9,22 +9,42 @@ const [firstArea, secondArea] = document.querySelectorAll(
 	".latest-news__rolling"
 );
 
-// <a href="${firstNews[0].href}" class="latest-news__rolling--prev">
-// ${firstNews[0].text}
-// </a>
-
-let idx = 0;
-setInterval(() => {
-	if (idx === 5) idx = 0;
-	console.log(idx);
-	aaa(idx);
-	idx += 1;
-}, 4000);
-
-function aaa(idx) {
+function createLatestNews(idx) {
 	firstArea.innerHTML = `
-    <a href="${firstNews[idx].href}" class="latest-news__rolling--current">
+    <a href="${firstNews[idx].href}" class="latest-news__rolling--befor-hover">
     ${firstNews[idx].text}
     </a>
     `;
 }
+
+function startNewChanging() {
+	interval = setInterval(() => {
+		if (idx === 4) idx = 0;
+		else idx += 1;
+		createLatestNews(idx);
+	}, 4000);
+}
+
+function stopNewChanging() {
+	clearInterval(interval);
+	const currentNews = document.querySelector(
+		".latest-news__rolling > a"
+	);
+	currentNews.classList = "latest-news__rolling--stop";
+}
+
+firstArea.addEventListener("mouseleave", () => {
+	console.log("아웃");
+	const currentNews = document.querySelector(
+		".latest-news__rolling--stop"
+	);
+	currentNews.classList = "latest-news__rolling--after-hover";
+	startNewChanging();
+});
+firstArea.addEventListener("mouseenter", stopNewChanging);
+
+let interval;
+let idx = 0;
+
+createLatestNews(idx);
+startNewChanging();
