2528bb547a3fa3159c392b6e2dd5dc0ac5f5dc32
mi-hye <satge13@gmail.com>
2024-03-11 11:17:56 +0900

    Setting: Change git setting case-sensitive

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-import newsHeader from "./components/newsHeader/newsHeader.js";
+import NewsHeader from "./components/newsHeader/NewsHeader.js";
 import initLatestNews from "./components/latest-news/latestNews.js";
 import Grid from "./components/press/Grid.js";
 import togglePressNaviTab from "./components/press/pressNavigation.js";
 
-newsHeader.newsHeaderReload();
-newsHeader.initDate();
+NewsHeader.newsHeaderReload();
+NewsHeader.initDate();
 initLatestNews();
 Grid.init();
 togglePressNaviTab();
