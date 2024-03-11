2eb2799a731a076a1999b44371d167e08d71a9d9
mi-hye <satge13@gmail.com>
2024-03-11 11:58:52 +0900

    refactor: Change file's name

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import NewsHeader from "./components/newsHeader/NewsHeader.js";
-import initLatestNews from "./components/latest-news/latestNews.js";
+import initLatestNews from "./components/latestNews/latestNews.js";
 import Grid from "./components/press/Grid.js";
 import togglePressNaviTab from "./components/press/pressNavigation.js";
 
