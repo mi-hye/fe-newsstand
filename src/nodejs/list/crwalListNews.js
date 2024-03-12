e3b1fda8f958d9ea3024454b13c8290104824343
mi-hye <satge13@gmail.com>
2024-03-12 22:17:06 +0900

    feat: Add saveJson

--- src/nodejs/list/crwalListNews.js
+++ src/nodejs/list/crwalListNews.js
@@ -1,11 +1,12 @@
 import puppeteer from "puppeteer";
 import getSingleNews from "./getSingleNews.js";
+import saveJSON from "../File.js";
 
 const LIST_BUTTON_TRIGGER = ".ContentPagingView-module__btn_view_list___j7eNR";
 const NEXT_NEWS_TRIGGER = ".ContentPagingView-module__btn_next___ZBhby";
-// const LIST_TABS = ".MediaO+ptionView-module__link_item___thVcT";
-
+const LIST_TABS = ".MediaOptionView-module__link_item___thVcT";
 const TOTAL_NEWS_SPAN = ".ContentPagingView-module__total___HUvt2";
+const CATEGORY_COUNT = 7;
 
 const getTotalNewsCount = async (page) =>
 	await page.evaluate((TOTAL_NEWS_SPAN) => {
@@ -17,34 +18,50 @@ const getTotalNewsCount = async (page) =>
 async function getCategoryTotalNews(page, totalCount) {
 	await page.waitForSelector(NEXT_NEWS_TRIGGER);
 
-	const arr = [];
-	for (let i = 0; i < totalCount; i++) {
-		const news = await getSingleNews(page);
-		arr.push(news);
-		await page.click(NEXT_NEWS_TRIGGER);
-	}
+	const categoryNews = await Array.from({ length: totalCount }).reduce(
+		async (prevPromise, curr) => {
+			const prev = await prevPromise;
+			curr = await getSingleNews(page);
+			prev.push(curr);
+			await page.click(NEXT_NEWS_TRIGGER);
+			return prev;
+		},
+		[]
+	);
 
-	console.log(arr);
-	// const result = await Array.from({ length: totalCount - 1 }).reduce(async (prevPromise, curr) => {
-	// 	const prev = await prevPromise;
-	// 	curr = await getSingleNews(page);
-	// 	prev.push(curr);
-	// 	await page.click(NEXT_NEWS_TRIGGER);
-	// 	return prev;
-	// }, []);
-	// console.log(result.length);
+	return categoryNews;
 }
 
+const getCategory = async (page) => {
+	await page.waitForSelector(LIST_TABS);
+	const category = await page.evaluate((LIST_TABS) => {
+		const categoryTabs = document.querySelectorAll(LIST_TABS);
+		return Array.from(categoryTabs).filter((category) => category.ariaSelected === "true")[0]
+			.innerText;
+	}, LIST_TABS);
+	return category;
+};
+
+async function getAllNews(page) {
+	return Array.from({ length: CATEGORY_COUNT }).reduce(async (prevPromise, _) => {
+		const prev = await prevPromise;
+		const totalCount = await getTotalNewsCount(page);
+		const category = await getCategory(page);
+		const categoryNews = await getCategoryTotalNews(page, totalCount);
+		prev.push({ [`${category}`]: categoryNews });
+		return prev;
+	}, []);
+}
 async function crawlListNews() {
 	const browser = await puppeteer.launch({ headless: false });
 	const page = await browser.newPage();
 	await page.goto("https://www.naver.com/");
-
 	await page.click(LIST_BUTTON_TRIGGER);
-	const totalCount = await getTotalNewsCount(page);
-	const total = await getCategoryTotalNews(page, totalCount);
-	// console.log(total);
-	// browser.close();
+
+	const allListNews = await getAllNews(page);
+	browser.close();
+	return allListNews;
 }
 
-await crawlListNews();
+const allListNews = await crawlListNews();
+saveJSON("allListNews", allListNews);
