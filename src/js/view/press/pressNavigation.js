8a5b67d24e2cecd2d6c5de04f2e0cd635d0ccf36
mi-hye <satge13@gmail.com>
2024-03-13 23:29:07 +0900

    feat: Add click first category when list icon cliked

--- src/js/view/press/pressNavigation.js
+++ src/js/view/press/pressNavigation.js
@@ -8,6 +8,11 @@ const addActive = (icon, wrap) => {
 	wrap.classList.add("active");
 };
 
+const clickFirstCategory = () => {
+	const firstCategory = document.querySelector(".press__list__nav__item");
+	firstCategory.click();
+};
+
 function togglePressNaviTab() {
 	const [listIcon, gridIcon] = document.querySelectorAll(".press__nav__icons-column i");
 	const [gridWrap, listWrap] = document.querySelectorAll(".press > div");
@@ -15,6 +20,7 @@ function togglePressNaviTab() {
 	listIcon.addEventListener("click", () => {
 		addActive(listIcon, listWrap);
 		removeActive(gridIcon, gridWrap);
+		clickFirstCategory();
 	});
 
 	gridIcon.addEventListener("click", () => {
