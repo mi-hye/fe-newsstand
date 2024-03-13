f194ba2eb2eba0a5d1bc3c37f281ffcf5cc54280
mi-hye <satge13@gmail.com>
2024-03-13 13:39:56 +0900

    feat: Add event When the tab clicked

--- src/js/view/list/listTab.js
+++ src/js/view/list/listTab.js
@@ -1,9 +1,13 @@
 function addEventListTab() {
-	const tabs = document.querySelector(".press__list__nav");
-	tabs.addEventListener("click", handleTabClick);
+	const tabList = document.querySelector(".press__list__nav");
+	tabList.addEventListener("click", handleTabClick);
 }
 
 function handleTabClick(e) {
+	const tabs = document.querySelectorAll(".press__list__nav__item");
+	tabs.forEach((tab) => {
+		tab.ariaSelected = false;
+	});
 	const currentTab = e.target;
 	const li = currentTab.closest("li");
 	li.ariaSelected = true;
