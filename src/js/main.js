d4efa59a5e25e9511d56a57bc2a1f5eeead4b841
mi-hye <satge13@gmail.com>
2024-03-13 20:21:04 +0900

    feat: Implement list news's tab

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,11 @@ import NewsHeader from "./view/newsHeader/NewsHeader.js";
 import initLatestNews from "./view/latestNews/latestNews.js";
 import Grid from "./view/press/Grid.js";
 import togglePressNaviTab from "./view/press/pressNavigation.js";
-import addEventListTab from "./view/list/listTab.js";
+import initListNews from "./view/list/initListNews.js";
 
 NewsHeader.reload();
 NewsHeader.initDate();
 initLatestNews();
 Grid.init();
 togglePressNaviTab();
-addEventListTab();
+initListNews();
--- src/js/view/list/listTab.js
+++ src/js/view/list/listTab.js
@@ -1,16 +1,30 @@
-function addEventListTab() {
+import { LIST_TAB } from "../../utils/Constants.js";
+
+function renderTab(allNewsJson) {
 	const tabList = document.querySelector(".press__list__nav");
-	tabList.addEventListener("click", handleTabClick);
+	tabList.innerHTML = LIST_TAB.reduce((prev, curr) => {
+		prev += `<li class="press__list__nav__item" role="tab" tabindex="0">
+		<span>${curr}</span>
+		<span class="press__list__nav__item__new-count curr">1&nbsp;</span>
+		<span class="press__list__nav__item__new-count total">&nbsp;/ 81</span>
+	</li>
+`;
+		return prev;
+	}, "");
+
+	addProgressEventListTab(tabList);
 }
 
-function handleTabClick(e) {
-	const tabs = document.querySelectorAll(".press__list__nav__item");
-	tabs.forEach((tab) => {
-		tab.ariaSelected = false;
+function addProgressEventListTab(tabList) {
+	tabList.addEventListener("click", (e) => {
+		const tabs = document.querySelectorAll(".press__list__nav__item");
+		tabs.forEach((tab) => {
+			tab.ariaSelected = false;
+		});
+		const currentTab = e.target;
+		const li = currentTab.closest("li");
+		li.ariaSelected = true;
 	});
-	const currentTab = e.target;
-	const li = currentTab.closest("li");
-	li.ariaSelected = true;
 }
 
-export default addEventListTab;
+export default renderTab;
