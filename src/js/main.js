4138ba1747e361f45f7b55bf6e695694abb4bd05
mi-hye <satge13@gmail.com>
2024-03-19 18:26:40 +0900

    design: Create modal

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import initLatestNews from "./view/latestNews/latestNews.js";
 import toggleDisplayTabs from "./view/viewTabs/display.js";
 import togglePressViewTabs from "./view/viewTabs/pressView.js";
 import { changeView } from "./view/viewTabs/store.js";
+import Grid from "./view/press/Grid.js";
 
 NewsHeader.reload();
 NewsHeader.initDate();
@@ -10,3 +11,4 @@ initLatestNews();
 toggleDisplayTabs();
 togglePressViewTabs();
 changeView();
+Grid.clickSubscribe();
