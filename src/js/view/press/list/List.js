2d346ef7ed43d60ef2c05a8a34f6e184f4dc6e68
mi-hye <satge13@gmail.com>
2024-03-20 20:59:27 +0900

    refactor: Change position animation reset

--- src/js/view/press/list/List.js
+++ src/js/view/press/list/List.js
@@ -2,35 +2,49 @@ import { getJson } from "../../../utils/fetchJson.js";
 import ListRenderer from "./renderer/ListRenderer.js";
 
 const List = {
+	binding: () => {},
 	totalList: await getJson("totalList"),
+	$tabList: document.querySelector(".press__list__nav"),
+	$currTab: "",
 	async init() {
 		const totalList = await getJson("totalList");
-		const $tabList = document.querySelector(".press__list__nav");
-		List.clickTab(totalList, $tabList);
+		List.clickTab(totalList, List.$tabList);
 	},
 	async totalRender() {
 		const totalList = await getJson("totalList");
-		const $tabList = document.querySelector(".press__list__nav");
 		const a = totalList.totalNews; // 246개뉴스
-		ListRenderer.totalTab($tabList);
+		ListRenderer.totalTab(List.$tabList);
 
-		List.clickTab(totalList, $tabList); // TODO 나중에 main으로 빼야함
+		List.clickTab(totalList, List.$tabList); // TODO 나중에 main으로 빼야함
 
 		const firstCategory = document.querySelector(".press__list__nav__item");
 		firstCategory.click();
 		ListRenderer.totalNews(totalList, 0);
 	},
-	nextNewsRender: (idx) => ListRenderer.totalNews(List.totalList, idx),
-	clickTab(totalList, $tabList) {
+	nextNewsRender(idx) {
+		ListRenderer.totalNews(List.totalList, idx);
+		List.resetAnimation(List.$currTab);
+	},
+	clickTab() {
+		const callback = ({ target }) => {
+			List.$currTab = List.handleProgressEvent(target);
+			ListRenderer.totalTabInfo(List.totalList, List.$currTab);
+			List.resetAnimation(List.$currTab);
+		};
+
 		// const intervalBox = ["dumy"];
-		$tabList.addEventListener("click", ({ target }) => {
-			// clearInterval(intervalBox[0]);
-			const $currTab = List.handleProgressEvent(target);
-			ListRenderer.totalTabInfo(totalList, $currTab);
-			// const current = handleProgressEvent(target);
-			// const { currNews, renderNextNews } = renderCategoryNews(current, allNewsJson, intervalBox);
-			// controlSwiper(LIST.firstPageIdx, LIST.lastPageIdx(currNews), renderNextNews);
-		});
+		List.$tabList.removeEventListener("click", List.binding);
+		List.binding = callback;
+		List.$tabList.addEventListener("click", List.binding);
+
+		// ({ target }) => {
+		// clearInterval(intervalBox[0]);
+		// $currTab = List.handleProgressEvent(target);
+		// ListRenderer.totalTabInfo(totalList, $currTab);
+		// const current = handleProgressEvent(target);
+		// const { currNews, renderNextNews } = renderCategoryNews(current, allNewsJson, intervalBox);
+		// controlSwiper(LIST.firstPageIdx, LIST.lastPageIdx(currNews), renderNextNews);
+		// });
 	},
 	handleProgressEvent(target) {
 		const tabs = document.querySelectorAll(".press__list__nav__item");
@@ -39,6 +53,12 @@ const List = {
 		const li = currentTab.closest("li");
 		li.ariaSelected = true;
 		return li;
+	},
+	resetAnimation($currTab) {
+		const animation = $currTab.querySelector(".press__list__nav__item--animation");
+		animation.classList.remove("active");
+		void animation.offsetWidth;
+		animation.classList.add("active");
 	},
 };
 
