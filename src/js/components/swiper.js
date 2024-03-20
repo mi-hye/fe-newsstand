e4326eb9efe3a0296080ddcda15751187c52ae87
mi-hye <satge13@gmail.com>
2024-03-20 20:01:20 +0900

    refactor: Add to delete previous click event of swiper

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,18 +1,44 @@
 import SwiperVisibility from "./SwiperVisibility.js";
 
+let binding;
+
 function controlSwiper(firstPageIdx, lastPageIdx, render, isGrid) {
 	const swiper = document.querySelector(".press__swiper");
 	SwiperVisibility.init(isGrid, firstPageIdx, lastPageIdx);
-	let currIdx = firstPageIdx;
+	let currIdx = 0;
 
-	swiper.addEventListener("click", async (e) => {
-		if (e.target.id === "right" && currIdx !== lastPageIdx) currIdx += 1;
-		if (e.target.id === "left" && currIdx) currIdx -= 1;
+	const callback = async (e) => {
+		const param = { firstPageIdx, lastPageIdx, render, isGrid };
+		currIdx = await handleClickSwiper(e.target, currIdx, param);
+		console.log("확인", render);
+	};
 
-		isGrid
-			? await swipeGrid(currIdx, firstPageIdx, lastPageIdx, render)
-			: swipeList(e, currIdx, firstPageIdx, lastPageIdx, render);
-	});
+	// swiper.removeEventListener("click", ({ target }) =>
+	// binding(target, currIdx, firstPageIdx, lastPageIdx, render, isGrid)
+	// );
+	swiper.removeEventListener("click", binding);
+	binding = callback;
+	swiper.addEventListener("click", binding);
+	// async ({ target }) => {
+	// currIdx = await handleClickSwiper(target, currIdx, firstPageIdx, lastPageIdx, render, isGrid);
+	// if (target.id === "right" && currIdx !== lastPageIdx) currIdx += 1;
+	// if (target.id === "left" && currIdx) currIdx -= 1;
+
+	// if (isGrid) await swipeGrid(currIdx, firstPageIdx, lastPageIdx, render);
+	// else await swipeList(currIdx, render);
+	// }
+	// );
+}
+
+async function handleClickSwiper(target, currIdx, param) {
+	const { firstPageIdx, lastPageIdx, render, isGrid } = param;
+	if (target.id === "right" && currIdx !== lastPageIdx) currIdx += 1;
+	if (target.id === "left" && currIdx) currIdx -= 1;
+
+	if (isGrid) await swipeGrid(currIdx, firstPageIdx, lastPageIdx, render);
+	else await swipeList(currIdx, render);
+
+	return currIdx;
 }
 
 async function swipeGrid(currIdx, firstPageIdx, lastPageIdx, render) {
@@ -20,28 +46,8 @@ async function swipeGrid(currIdx, firstPageIdx, lastPageIdx, render) {
 	SwiperVisibility.set(currIdx, firstPageIdx, lastPageIdx);
 }
 
-function swipeList(e, currIdx, firstPageIdx, lastPageIdx, render) {
-	// 	const currSwiper = e.target.id;
-	// 	const listTabs = document.querySelectorAll(".press__list__nav__item");
-	// 	const currTabIdx = Array.from(listTabs)
-	// 		.map((v) => v.ariaSelected)
-	// 		.indexOf("true");
-	// 	if (currIdx === firstPageIdx && currSwiper === "left") {
-	// 		//0일때 왼쪽이면 탭변경
-	// 		clickNextListTab(currTabIdx, listTabs);
-	// 	}
-	// 	if (currIdx === lastPageIdx && currSwiper === "right") {
-	// 		//마지막일때 오른쪽이면 탭변경
-	// 		clickNextListTab(currTabIdx, listTabs);
-	// 	}
-	// 	render(currIdx);
+async function swipeList(currIdx, render) {
+	await render(currIdx);
 }
-
-// function clickNextListTab(currTabIdx, listTabs) {
-// 	let nextTabIdx = currTabIdx + 1;
-// 	if (currTabIdx === 0) nextTabIdx = 6;
-// 	if (currTabIdx === 6) nextTabIdx = 0;
-// 	listTabs[nextTabIdx].click();
-// }
 
 export default controlSwiper;
