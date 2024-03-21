dca5ca35806b6f3c51b64f001e95bf38bdc73a2f
mi-hye <satge13@gmail.com>
2024-03-21 23:37:53 +0900

    refactor: Delete interval init

--- src/js/view/latestNews/Rolling.js
+++ src/js/view/latestNews/Rolling.js
@@ -1,12 +1,10 @@
 import { ROLLING } from "../../utils/Constants.js";
 
-//TODO class Rolling 은 constructor 에서 interval 이라도 파라미터로 받아서 범용성 올리기.
 class Rolling {
 	#interval;
 	idx;
 
 	constructor() {
-		this.#interval = "";
 		this.idx = ROLLING.firstNewsIdx;
 	}
 	startNewsRolling(delay, createLatestNews) {
